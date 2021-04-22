e5bf746fd39c84767a134aa419f1c76110b409a8
Winner Bamgboye <bamgboyewinner@gmail.com>
2021-04-22 14:41:24 +0100

    using dictionaries in python - first deployment

---
+++ dictionary.py
@@ -0,0 +1,32 @@
+#register
+#login
+
+
+
+#Dictionary
+
+
+#method1
+dictionaryOne = {
+    'key1':'value1',
+    'key2':'value2',
+    'key3':'value3'
+}
+
+#method2
+dictionaryTwo = {}
+dictionaryTwo['key4'] = 'value4'
+dictionaryTwo['key5'] = 'value5'
+dictionaryTwo['key6'] = 'value6'
+
+#print(dictionaryTwo)
+#print(aSampleList)
+#del dictionaryTwo["key4"]
+#dictionaryOne.pop('key1')
+#print(dictionaryOne)
+
+
+#dictionaryLoop
+
+for key,value in dictionaryOne.items():
+    print("I have " + key + " relating with " + value)
